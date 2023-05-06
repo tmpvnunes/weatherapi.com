9b9811f7e205d92a5e2c5284d07af08c87432790
tmpvnunes <tmpvnunes@gmail.com>
2023-05-06 02:33:05 +0100

    Add current weather list

--- src/main/java/com/codegate01/weatherapi/entity/Location.java
+++ src/main/java/com/codegate01/weatherapi/entity/Location.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "location")
